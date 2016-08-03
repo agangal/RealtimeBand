b4eae6d08a5b0384cd2d4dfb68490881f9eb4943
agangal <ashish.gangal1990@gmail.com>
2016-08-02 23:27:53 -0700

    Pushing it all.

--- BarControl/BarChartControl.cs
+++ BarControl/BarChartControl.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
@@ -13,11 +15,92 @@ using Windows.UI.Xaml.Media;
 
 namespace BarControl
 {
+    [TemplatePart(Name = ContainerPartName, Type = typeof(Grid))]
     public sealed class BarChartControl : Control
     {
+        private Compositor _compositor;
+        private ContainerVisual _root;
+        private const string ContainerPartName = "PART_Container";
+        private SpriteVisual _needle;
+
         public BarChartControl()
         {
             this.DefaultStyleKey = typeof(BarChartControl);
+        }
+
+        public static readonly DependencyProperty MinimumProperty = DependencyProperty.Register("Minimum", typeof(double), typeof(BarChartControl), new PropertyMetadata(0.0));
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register("Maximum", typeof(double), typeof(BarChartControl), new PropertyMetadata(4000.0));
+        public static readonly DependencyProperty ScaleWidthProperty = DependencyProperty.Register("ScaleWidth", typeof(double), typeof(BarChartControl), new PropertyMetadata(26.0));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(BarChartControl), new PropertyMetadata(0.0, OnValueChanged));
+        public static readonly DependencyProperty RightBrushProperty = DependencyProperty.Register("RightBrush", typeof(Brush), typeof(BarChartControl), new PropertyMetadata(new SolidColorBrush(Colors.Orange)));
+        public static readonly DependencyProperty LeftBrushProperty = DependencyProperty.Register("TrailBrush", typeof(Brush), typeof(BarChartControl), new PropertyMetadata(new SolidColorBrush(Colors.Orange)));
+
+        /// <summary>
+        /// Gets or sets the minimum value of the control
+        /// </summary>
+        public double Minimum
+        {
+            get { return (double)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Maximum value of the control
+        /// </summary>
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the ScaleWidth property
+        /// </summary>
+        public double ScaleWidth
+        {
+            get { return (double)GetValue(ScaleWidthProperty); }
+            set { SetValue(ScaleWidthProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Value property
+        /// </summary>
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public Brush RightBrush
+        {
+            get { return (SolidColorBrush)GetValue(RightBrushProperty); }
+            set { SetValue(RightBrushProperty, value); }
+        }
+
+        public Brush LeftBrush
+        {
+            get { return (SolidColorBrush)GetValue(LeftBrushProperty); }
+            set { SetValue(LeftBrushProperty, value); }
+        }
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            OnValueChanged(d);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            var container = this.GetTemplateChild(ContainerPartName)
+            base.OnApplyTemplate();
+        }
+
+        private static void OnValueChanged(DependencyObject d)
+        {
+            BarChartControl c = (BarChartControl)d;
+            if(!Double.IsNaN(c.Value))
+            {
+
+            }
         }
     }
 }
